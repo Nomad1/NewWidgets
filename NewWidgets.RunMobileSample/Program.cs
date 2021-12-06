97c28723545d2b648e9ccafbf9aa12989230929e
Alexei Garbuzenko <gorbuz@gmail.com>
2021-12-06 14:16:20 -0500

    Descriptive comments, some call changes

--- NewWidgets.RunMobileSample/Program.cs
+++ NewWidgets.RunMobileSample/Program.cs
@@ -6,6 +6,10 @@ using RunMobile.Utility;
 
 namespace NewWidgets.RunMobileSample
 {
+    /// <summary>
+    /// This is a simple class implementing DesktopWindow (system level OpenGL window) in order to load two shaders and display
+    /// a TestWindow class
+    /// </summary>
     class SampleWindow : RunMobile.OpenTK.OpenTKDesktopWindow
     {
         private TestWindow m_window;
@@ -15,9 +19,14 @@ namespace NewWidgets.RunMobileSample
         {
         }
 
+        /// <summary>
+        /// Engine initialization. We need to set resource path there and load shaders.
+        /// </summary>
         protected override void Init()
         {
             AssetManager.Init("assets", ".");
+
+            // we need to load at least default UI sprite shader
 
             LoadShader("ui",
                         0,
@@ -26,45 +35,56 @@ namespace NewWidgets.RunMobileSample
                         DefaultSpriteShaderUniforms
                         );
 
+            // font shader theoretically could be combined with sprite shader
+            // but it's better to have a separate one
+
             LoadShader("font",
                         0,
                         "simple_sprite.vert.shader",
                         "font.frag.shader",
                         DefaultSpriteShaderUniforms
                         );
-
-            if (m_gameController == null)
-            {
-                m_gameController = new BaseGame(m_data);
-                m_gameController.OnInit += HandleGameInit;
-            }
         }
 
+        /// <summary>
+        /// Creation of game controller. We need to override it to hook onto OnInit method
+        /// </summary>
+        protected override BaseGameController CreateGameController()
+        {
+            BaseGameController gameController = new BaseGameController(UserData);
+            gameController.OnInit += HandleGameInit;
+
+            return gameController;
+        }
+
+        /// <summary>
+        /// This method is called only once to create game windows and load textures
+        /// </summary>
         private void HandleGameInit()
         {
-            SpriteManager.Instance.LoadSpriteAtlas("ui");
-            SpriteManager.Instance.LoadSpriteAtlas("font5");
-            WidgetManager.Init(0.5f);
+            SpriteManager.Instance.LoadSpriteAtlas("ui"); // loads ui.bin with defulat ui atlas
+            SpriteManager.Instance.LoadSpriteAtlas("font5"); // loads font5.bin with font MSDF atlas
+            WidgetManager.Init(0.5f); // 0.5 is font scale
             WidgetManager.LoadUI(AssetManager.GetAssetTextFile("ui.xml"));
 
+            // Here we're creating main window and it's added to the Game Controller
             m_window = new TestWindow();
-            m_gameController.AddWindow(m_window);
+            GameController.AddWindow(m_window);
         }
 
         protected override void OnUpdateFrame(OpenTK.FrameEventArgs e)
         {
             base.OnUpdateFrame(e);
 
+            // Pass FPS values to text label in the TestWindow class
             if (m_window != null)
-                //m_window.SetFpsValue((float)UpdateFrequency, (float)RenderFrequency);
                 m_window.SetFpsValue(UpdateFPS, DrawFPS);
         }
 
         public static void Main(string[] args)
         {
             SampleWindow window = new SampleWindow();
-            //window.Run(60,60);
-            window.Run(0.0f);
+            window.Run(60, 60); // 60 FPS for update and rendering should be ok
         }
     }
 }
